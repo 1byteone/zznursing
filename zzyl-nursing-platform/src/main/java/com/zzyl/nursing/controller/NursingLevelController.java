61b6bc1e00ad84b630ccc0c213e0603d47bc0f41
ychenchen <ycc@itcast.cn>
2025-06-12 21:22:05 +0800

    入住办理-后端接口开发

--- zzyl-nursing-platform/src/main/java/com/zzyl/nursing/controller/NursingLevelController.java
+++ zzyl-nursing-platform/src/main/java/com/zzyl/nursing/controller/NursingLevelController.java
@@ -114,4 +114,10 @@ public class NursingLevelController extends BaseController
     {
         return toAjax(nursingLevelService.deleteNursingLevelByIds(ids));
     }
+
+    @GetMapping("/all")
+    public R<List<NursingLevel>> listAll()
+    {
+    	return R.ok(nursingLevelService.listAll());
+    }
 }
