8f0458d234d23c27392e7e567452bc4ee5286234
ychenchen <ycc@itcast.cn>
2025-06-16 11:56:59 +0800

    定时任务、定时更新合同状态

---
+++ zzyl-nursing-platform/src/main/java/com/zzyl/nursing/task/HelloTask.java
@@ -0,0 +1,10 @@
+package com.zzyl.nursing.task;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloTask {
+    public void myTask() {
+        System.out.println("HelloTask");
+    }
+}
