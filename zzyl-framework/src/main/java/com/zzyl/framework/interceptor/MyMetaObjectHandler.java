ea5c6cdc382180ed044d642f7bd9356d7c198e30
YangJs <yjs_0831@qq.com>
2026-03-28 17:40:18 +0800

    第13章 智能监测-数据处理展示 3. 设备数据转发 3.2 SDK改造 3.2.2 导入pom依赖 <!-- amqp 1.0 qpid client --> <dependency>     <groupId>org.apache.qpid</groupId>     <artifactId>qpid-jms-client</artifactId>     <version>0.61.0</version> </dependency> 3.2.4 实现ApplicationRunner接口之后的AmqpClient 4. 接收设备端数据 4.3 基础代码生成 接收到的数据格式如下，是一个json字符串，咱们可以定义一个类来转换接收数据 {   "resource": "device.property",   "event": "report",   "event_time": "20250706T023712Z",   "event_time_ms": "2025-07-06T02:37:12.573Z",   "request_id": "c13e6473-2709-4f34-8017-3538d9c57ccd",   "notify_data": {     "header": {       "app_id": "a7bb0103499749d984777860b53eadf6",       "device_id": "6864b04ed582f2001837367e_watch01",       "node_id": "watch01",       "product_id": "6864b04ed582f2001837367e",       "gateway_id": "6864b04ed582f2001837367e_watch01"     },     "body": {       "services": [         {           "service_id": "watch_services",           "properties": {             "BodyTemp": 36.8,             "HeartRate": 5.9712353,             "xueyang": 84.60445,             "BatteryPercentage": 24.040668           },           "event_time": "20250706T023712Z"         }       ]     }   } } 2. 修改AmqpClient类，解析json数据并调用设备数据的业务层，批量添加数据 3. 在设备数据的业务层定义批量添加的方法 5. 查询设备的物模型数据

--- zzyl-framework/src/main/java/com/zzyl/framework/interceptor/MyMetaObjectHandler.java
+++ zzyl-framework/src/main/java/com/zzyl/framework/interceptor/MyMetaObjectHandler.java
@@ -8,20 +8,23 @@ import org.apache.commons.lang3.ObjectUtils;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
 
-    @Autowired
-    private HttpServletRequest request;
-
     @SneakyThrows
     public boolean isExclude() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            // 非 Web 请求上下文，默认不排除
+            return false;
+        }
+        HttpServletRequest request = attributes.getRequest();
         String requestURI = request.getRequestURI();
         if(requestURI.startsWith("/member")) {
             return true;
@@ -34,6 +37,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
         if(!isExclude()) {
             this.strictInsertFill(metaObject, "createBy", String.class, loadUserId() + "");
+
         }
 
     }
---
+++ zzyl-nursing-platform/src/main/java/com/zzyl/nursing/service/impl/DeviceDataServiceImpl.java
@@ -0,0 +1,192 @@
+package com.zzyl.nursing.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zzyl.common.constant.HttpStatus;
+import com.zzyl.common.core.page.TableDataInfo;
+import com.zzyl.common.utils.DateTimeZoneConverter;
+import com.zzyl.common.utils.DateUtils;
+import com.zzyl.nursing.domain.Device;
+import com.zzyl.nursing.dto.DeviceDataPageReqDto;
+import com.zzyl.nursing.mapper.DeviceMapper;
+import com.zzyl.nursing.task.vo.IotMsgNotifyData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.zzyl.nursing.mapper.DeviceDataMapper;
+import com.zzyl.nursing.domain.DeviceData;
+import com.zzyl.nursing.service.IDeviceDataService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * 设备数据Service业务层处理
+ * 
+ * @author yjs
+ * @date 2026-03-28
+ */
+@Service
+public class DeviceDataServiceImpl extends ServiceImpl<DeviceDataMapper, DeviceData> implements IDeviceDataService
+{
+    @Autowired
+    private DeviceDataMapper deviceDataMapper;
+
+    /**
+     * 查询设备数据
+     * 
+     * @param id 设备数据主键
+     * @return 设备数据
+     */
+    @Override
+    public DeviceData selectDeviceDataById(Long id)
+    {
+        return getById(id);
+    }
+
+    /**
+     * 查询设备数据列表
+     * 
+     * @param dto 设备数据
+     * @return 设备数据
+     */
+    @Override
+    public TableDataInfo  selectDeviceDataList(DeviceDataPageReqDto dto)
+    {
+        LambdaQueryWrapper<DeviceData> queryWrapper = new LambdaQueryWrapper<>();
+        Page<DeviceData> page = new Page(dto.getPageNum(), dto.getPageSize());
+        //模糊查询设备名称
+        if(ObjectUtil.isNotEmpty(dto.getDeviceName())){
+            queryWrapper.like(DeviceData::getDeviceName,dto.getDeviceName());
+        }
+        //精确查询功能名称
+        if(ObjectUtil.isNotEmpty(dto.getFunctionId())){
+            queryWrapper.eq(DeviceData::getFunctionId,dto.getFunctionId());
+        }
+        //时间范围查询
+        if(ObjectUtil.isNotEmpty(dto.getStartTime()) && ObjectUtil.isNotEmpty(dto.getEndTime())){
+            queryWrapper.between(DeviceData::getAlarmTime,dto.getStartTime(),dto.getEndTime());
+        }
+        
+        //分页查询
+        page = page(page,queryWrapper);
+        
+        //封装分页对象
+        return getTableDataInfo(page);
+    }
+
+    /**
+     * 封装分页对象
+     * @param page
+     * @return
+     */
+    private TableDataInfo getTableDataInfo(Page<DeviceData> page) {
+        TableDataInfo tableDataInfo = new TableDataInfo();
+        tableDataInfo.setCode(HttpStatus.SUCCESS);
+        tableDataInfo.setMsg("查询成功");
+        tableDataInfo.setRows(page.getRecords());
+        tableDataInfo.setTotal(page.getTotal());
+        return tableDataInfo;
+
+    }
+
+    /**
+     * 新增设备数据
+     * 
+     * @param deviceData 设备数据
+     * @return 结果
+     */
+    @Override
+    public int insertDeviceData(DeviceData deviceData)
+    {
+        return save(deviceData) ? 1 : 0;
+    }
+
+    /**
+     * 修改设备数据
+     * 
+     * @param deviceData 设备数据
+     * @return 结果
+     */
+    @Override
+    public int updateDeviceData(DeviceData deviceData)
+    {
+        return updateById(deviceData) ? 1 : 0;
+    }
+
+    /**
+     * 批量删除设备数据
+     * 
+     * @param ids 需要删除的设备数据主键
+     * @return 结果
+     */
+    @Override
+    public int deleteDeviceDataByIds(Long[] ids)
+    {
+        return removeByIds(Arrays.asList(ids)) ? 1 : 0;
+    }
+
+    /**
+     * 删除设备数据信息
+     * 
+     * @param id 设备数据主键
+     * @return 结果
+     */
+    @Override
+    public int deleteDeviceDataById(Long id)
+    {
+        return removeById(id) ? 1 : 0;
+    }
+
+    @Autowired
+    private DeviceMapper deviceMapper;
+
+    /**
+     * 批量插入设备数据
+     * @param iotMsgNotifyData
+     */
+    @Override
+    public void batchInsertDeviceData(IotMsgNotifyData iotMsgNotifyData) {
+        String iotId = iotMsgNotifyData.getHeader().getDeviceId();
+        //查询设备信息
+        Device device = deviceMapper.selectOne(Wrappers.<Device>lambdaQuery().eq(Device::getIotId, iotId));
+        if(ObjectUtil.isEmpty( device)){
+            log.error("设备不存在");
+            return;
+        }
+        //批量保存设备数据
+        iotMsgNotifyData.getBody().getServices().forEach(s->{
+            //判断属性是否为空
+            Map<String,Object> properties = s.getProperties();
+            if(ObjectUtil.isEmpty(properties)){
+                return;
+            }
+
+            //上报时间处理
+            String eventTimeStr = s.getEventTime();
+            LocalDateTime localDateTime = LocalDateTimeUtil.parse(eventTimeStr,"yyyyMMdd'T'HHmmss'Z'");
+            LocalDateTime eventTime = DateTimeZoneConverter.utcToShanghai(localDateTime);
+
+            List<DeviceData> list = new ArrayList<>();
+
+            // key:属性id，value:属性值
+            properties.forEach((k,v)->{
+                DeviceData deviceData = BeanUtil.toBean(device, DeviceData.class);
+                deviceData.setId( null);
+                deviceData.setAlarmTime(eventTime);
+                deviceData.setFunctionId( k);
+                deviceData.setDataValue( v.toString());
+                list.add(deviceData);
+            });
+            //批量保存设备数据
+            saveBatch(list);
+        });
+    }
+}
